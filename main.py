ed7940bd58b22baef2a6e2aef673a565193d086b
Katarzyna <katarzyna.kopowska@gmail.com>
2020-02-19 14:47:18 +0100

    Create BattlePetScrapperManager class

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-from ah_api_client import RealmScraper
-from ah_api_client import BattlePetScraper
+from ah_api_client import BattlePetsScraperManager
 
 
 if __name__ == "__main__":
-    realm_scraper = RealmScraper()
-    realm_data = realm_scraper.get()
-    data_list = []
-
-    for data in realm_data.values():
-        realm = realm_scraper.parse(data)
-        data_list.append(realm)
-
-    for realm in data_list:
-        if realm.house_id == 180:
-            print(f'{realm.name}, {realm.href}, {realm.house_id}')
-
-    battle_pet = BattlePetScraper()
-    battle_pet_data = battle_pet.get(house=180)
-
-    battle_pet_list = []
-    for data in battle_pet_data.values():
-        pet = battle_pet.parse(data)
-        battle_pet_list.append(pet)
-
-    print("List of battle pets for realm TARREN MILL/DENTARG:")
-    for pet_data in battle_pet_list:
-        print(f"'{pet_data.name}' price: {pet_data.price/1000}" '\n')
+    scraper_manager = BattlePetsScraperManager()
+    realm_data = scraper_manager.run()
