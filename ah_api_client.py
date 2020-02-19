b5a55aa7fc5c209d4d64de41e9e342d39ee5009e
Katarzyna <katarzyna.kopowska@gmail.com>
2020-02-19 09:02:28 +0100

    Create 'get_realm_list' method

--- ah_api_client.py
+++ ah_api_client.py
@@ -4,32 +4,25 @@ import requests
 
 PetInformation = collections.namedtuple("Pet", ("name", "realm", "price")
                                         )
-Realm = collections.namedtuple("Realm", ("name", "region")
-                               )
+
 
 class RealmScraper:
     url = "https://theunderminejournal.com/api/realms.php"
-    data_class = Realm
 
     def get_realm_information(self):
         realm_information = requests.get(self.url).json()['realms'][1]
         return realm_information
 
-    def parse_realm_information(self, realm_information):
-        data = {
-            "name": realm_information["298"]["name"],
-            "region": realm_information["298"]["region"],
-        }
-
-        return self.data_class(**data)
+    def get_realm_list(self, realm_information):
+        realm_list = []
+        for key, value in realm_information.items():
+            realm_list.append(value['name'])
+        return realm_list
 
 
 if __name__ == "__main__":
     realm_scraper = RealmScraper()
 
     realm_info = realm_scraper.get_realm_information()
-    print(realm_scraper.parse_realm_information(realm_info))
-
-
-
-
+    x = realm_scraper.get_realm_list(realm_info)
+    print(x)
