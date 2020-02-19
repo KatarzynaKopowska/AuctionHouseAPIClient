3ec37f1b1f5db3b922eda98ae3f1929970962b2f
Katarzyna <katarzyna.kopowska@gmail.com>
2020-02-19 11:35:14 +0100

    Move BattlePetScraper class to ah_api_client.py file

--- ah_api_client.py
+++ ah_api_client.py
@@ -1,14 +1,17 @@
 import requests
 import collections
 
-Realm = collections.namedtuple("Realm", ("name", "href", "house_id"))
+Realm = collections.namedtuple("Realm", ("name", "href", "house_id")
+                               )
+BattlePet = collections.namedtuple("Pet", ("name", "realm", "price",)
+                                   )
 
 
 class BaseScraper:
     url = None
     data_class = None
 
-    def get(self):
+    def get(self, **kwargs):
         raise NotImplementedError
 
     def parse(self, realm_data):
@@ -19,13 +22,30 @@ class RealmScraper(BaseScraper):
     url = "https://theunderminejournal.com/api/realms.php"
     data_class = Realm
 
-    def get(self):
+    def get(self, **kwargs):
         return requests.get(self.url).json()['realms'][1]  # second list element contains information about EU realms
 
-    def parse(self, realm_data):
-        data = {
-            "name": realm_data.get("name"),
-            "href": realm_data.get("slug"),
-            "house_id": realm_data.get("house"),
+    def parse(self, data):
+        realm_data = {
+            "name": data.get("name"),
+            "href": data.get("slug"),
+            "house_id": data.get("house"),
         }
-        return self.data_class(**data)
+        return self.data_class(**realm_data)
+
+
+class BattlePetScraper(BaseScraper):
+    url = "https://theunderminejournal.com/api"
+    data_class = BattlePet
+
+    def get(self, **kwargs):
+        url = "{}/category.php?{}={}&id=battlepets".format(self.url, kwargs, kwargs)
+        battle_pet_data = requests.get(url, params=kwargs).json()
+        return battle_pet_data
+
+    def parse(self, data):
+        battle_pet_data = {
+
+
+        }
+        return self.data_class(**battle_pet_data)
